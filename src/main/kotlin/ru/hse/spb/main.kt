be4b78eea4fe949fb15c735d62c56982f4ce0b5b
aliscafo <ero.alina@gmail.com>
2018-09-11 00:49:44 +0100

    add solution

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,130 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class PetrLangUtils {
+    companion object {
+        const val MALE_ADJ = "lios"
+        const val MALE_NOUN = "etr"
+        const val MALE_VERB = "initis"
+        const val FEMALE_ADJ = "liala"
+        const val FEMALE_NOUN = "etra"
+        const val FEMALE_VERB = "inites"
+
+        fun ifWord(word: String): Boolean {
+            if (word.endsWith(MALE_ADJ) ||
+                    word.endsWith(MALE_NOUN) ||
+                    word.endsWith(MALE_VERB) ||
+                    word.endsWith(FEMALE_ADJ) ||
+                    word.endsWith(FEMALE_NOUN) ||
+                    word.endsWith(FEMALE_VERB)) {
+                return true
+            }
+            return false
+        }
+
+        fun ifMale(word: String): Boolean {
+            if (word.endsWith(MALE_ADJ) ||
+                    word.endsWith(MALE_NOUN) ||
+                    word.endsWith(MALE_VERB)) {
+                return true
+            }
+            return false
+        }
+
+        fun ifAdj(word: String): Boolean {
+            if (word.endsWith(MALE_ADJ) ||
+                    word.endsWith(FEMALE_ADJ)) {
+                return true
+            }
+            return false
+        }
+
+        fun ifNoun(word: String): Boolean {
+            if (word.endsWith(MALE_NOUN) ||
+                    word.endsWith(FEMALE_NOUN)) {
+                return true
+            }
+            return false
+        }
+
+        fun ifVerb(word: String): Boolean {
+            if (word.endsWith(MALE_VERB) ||
+                    word.endsWith(FEMALE_VERB)) {
+                return true
+            }
+            return false
+        }
+    }
+}
+
+class PetrLangChecker(var text: String?) {
+    var maleNumber = 0
+    var femaleNumber = 0
+    val words: List<String>
+    var curPos = 0
+    var isOneNoun = false
+
+    init {
+        words = text!!.split("\\s".toRegex())
+    }
+
+    fun noteGender(word: String) {
+        if (PetrLangUtils.ifMale(word)) {
+            maleNumber++
+        } else {
+            femaleNumber++
+        }
+    }
+
+    fun readAdjs() {
+        while (curPos < words.size && PetrLangUtils.ifAdj(words.get(curPos))) {
+            noteGender(words.get(curPos))
+            curPos++
+        }
+    }
+
+    fun readNouns() {
+        val startPos = curPos
+
+        while (curPos < words.size && PetrLangUtils.ifNoun(words.get(curPos))) {
+            noteGender(words.get(curPos))
+            curPos++
+        }
+
+        isOneNoun = curPos - startPos == 1
+    }
+
+    fun readVerbs() {
+        while (curPos < words.size && PetrLangUtils.ifVerb(words.get(curPos))) {
+            noteGender(words.get(curPos))
+            curPos++
+        }
+    }
+
+    fun checkFinalState(): Boolean {
+        if (curPos != words.size || (femaleNumber != 0 && maleNumber != 0) || isOneNoun.equals(false)) {
+            if (words.size == 1 && PetrLangUtils.ifWord(words.get(0))) {
+                return true
+            }
+            return false
+        }
+        return true
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val input = Scanner(System.`in`)
+    val text = input.nextLine()
+
+    val checker = PetrLangChecker(text)
+    checker.readAdjs()
+    checker.readNouns()
+    checker.readVerbs()
+
+    if (checker.checkFinalState().equals(true)) {
+        println("YES")
+    } else {
+        println("NO")
+    }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,54 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class TestSource {
+class Tests {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testIsWord() {
+        assertTrue(PetrLangUtils.ifWord("petr"))
+        assertFalse(PetrLangUtils.ifWord("animatis"))
+    }
+
+    @Test
+    fun testGenderMethod() {
+        assertTrue(PetrLangUtils.ifMale("petr"))
+        assertFalse(PetrLangUtils.ifMale("nataliala"))
+    }
+
+    @Test
+    fun testPartOfTheSpeechMethod() {
+        assertTrue(PetrLangUtils.ifNoun("petr"))
+        assertFalse(PetrLangUtils.ifNoun("lios"))
+
+        assertTrue(PetrLangUtils.ifAdj("nataliala"))
+        assertFalse(PetrLangUtils.ifAdj("petr"))
+
+        assertTrue(PetrLangUtils.ifVerb("feinites"))
+        assertFalse(PetrLangUtils.ifVerb("vetra"))
+    }
+
+    @Test
+    fun testCurState() {
+        var checker = PetrLangChecker("nataliala kataliala vetra feinites")
+        checker.readAdjs()
+        checker.readNouns()
+
+        assertEquals(0, checker.maleNumber)
+        assertEquals(3, checker.femaleNumber)
+        assertTrue(checker.isOneNoun)
+        assertEquals(3, checker.curPos)
+    }
+
+    @Test
+    fun testFinalStateMethod() {
+        var checker = PetrLangChecker("nataliala kataliala vetra vetra feinites")
+        checker.readAdjs()
+        checker.readNouns()
+        checker.readVerbs()
+
+        assertFalse(checker.checkFinalState())
     }
 }
